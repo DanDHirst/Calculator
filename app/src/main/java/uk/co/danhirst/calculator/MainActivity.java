25f298cbefd8d2e1f0946990722048aa6f49b0e9
Dan Hirst <danhirst123@gmail.com>
2019-06-16 19:46:27 +0100

    added on click listener for the buttons 0-9

--- app/src/main/java/uk/co/danhirst/calculator/MainActivity.java
+++ app/src/main/java/uk/co/danhirst/calculator/MainActivity.java
@@ -1,13 +1,68 @@
 package uk.co.danhirst.calculator;
 
+import android.net.sip.SipSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    private Double operand1;
+    private Double operand2;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (EditText) findViewById(R.id.result);
+        newNumber = (EditText) findViewById(R.id.newNum);
+        displayOperation = (TextView) findViewById(R.id.operation);
+
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button buttonEquals = (Button) findViewById(R.id.buttonEqual);
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        Button buttonMult = (Button) findViewById(R.id.buttonMult);
+        Button buttonDot = (Button) findViewById(R.id.buttonDot);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                newNumber.append(b.getText().toString());
+            }
+        };
+        button0.setOnClickListener(listener);
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+        buttonDot.setOnClickListener(listener);
+
     }
 }
